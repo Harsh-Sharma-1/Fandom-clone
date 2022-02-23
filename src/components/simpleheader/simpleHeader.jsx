beff3716819e98b4fd33ad265f2cd8f373cdbddf
Harsh-Sharma-1 <85671456+Harsh-Sharma-1@users.noreply.github.com>
2022-02-23 21:54:03 +0530

    project initialization

---
+++ src/components/simpleheader/simpleHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import fandom from "../../assets/fandom.png";
+const SimpleHeader = () => {
+  return (
+    <Container>
+      <Image src={fandom} />
+    </Container>
+  );
+};
+
+export default SimpleHeader;
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #ffc500;
+  padding: 10px 100px;
+`;
+
+const Image = styled.img`
+  width: 150px;
+`;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "react-redux";
+import SimpleHeader from "../components/simpleheader/simpleHeader";
+export default class Login extends Component {
+  render() {
+    return (
+      <>
+        <SimpleHeader />
+      </>
+    );
+  }
+}
